511ed3210a3c05076266003a9b433440f2fbddae
Andy Chen <andych3n@yahoo.com>
2021-04-03 19:38:05 -0400

    draft of setup script to test size limits

---
+++ setup.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS Movie;
+
+-- Describe schema for relation Movie
+CREATE TABLE Movie (
+  movieID   VARCHAR(15),
+  title     VARCHAR(300),
+  year      INT,
+  runtime   INT,
+  budget    DECIMAL(19,4),
+  revenue   DECIMAL(19,4),
+  budget_adj    DECIMAL(19,4),
+  revenue_adj   DECIMAL(19,4),
+  PRIMARY KEY(movieID)
+);
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/movie.txt' INTO TABLE Movie
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- test query
+SELECT *
+FROM Movie
+WHERE title = 'Pickup on South Street';
