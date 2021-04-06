42bd00cfa2517afbacaf2086ef6e4ca2e0a6e5a6
CnishComputing <70718607+CnishComputing@users.noreply.github.com>
2021-04-06 12:18:07 -0400

    Update setup-small.sql

--- setup-small.sql
+++ setup-small.sql
@@ -9,3 +9,53 @@ CREATE TABLE Movie (
   revenue_adj   DECIMAL(19,4),
   PRIMARY KEY(movieID)
 );
+
+CREATE TABLE Genre (
+  movieID   VARCHAR(15),
+  genre     VARCHAR(100),
+  PRIMARY KEY (genre, movieID),
+  FOREIGN KEY (movieID) REFERENCES Movie(movieID)
+);
+
+CREATE TABLE Keyword (
+  movieID   VARCHAR(15),
+  keyword   VARCHAR(100),
+  PRIMARY KEY(keyword, movieID),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+);
+
+CREATE TABLE ShootingLocation (
+  movieID   VARCHAR(15),
+  address   VARCHAR(500),
+  city      VARCHAR(100),
+  state     VARCHAR(100),
+  country   VARCHAR(100),
+  PRIMARY KEY(movieID, address, city, state, country),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+);
+
+CREATE TABLE Person (
+  personID  VARCHAR(15),
+  name      VARCHAR(200),
+  birthday  DATE,
+  PRIMARY KEY(personID)
+);
+
+CREATE TABLE DirectedBy (
+  personID  VARCHAR(15),
+  movieID   VARCHAR(15),
+  PRIMARY KEY(personID, movieID),
+  FOREIGN KEY(personID) REFERENCES Person(personID),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+);
+
+CREATE TABLE Starring (
+  personID  VARCHAR(15),
+  movieID   VARCHAR(15),
+  role      VARCHAR(200),
+  PRIMARY KEY(personID, movieID, role),
+  FOREIGN KEY(personID) REFERENCES Person(personID),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+);
+
+  
