f7f14299d1edb562484b3f7e5a3cc62319e98177
Andy Chen <andych3n@yahoo.com>
2021-04-07 01:43:15 -0400

    finalized data files, sql & python scripts, and txt files documenting process/description

--- setup-small.sql
+++ setup-small.sql
@@ -1,3 +1,6 @@
+-- Andy Chen, achen93
+-- Curtis Nishimoto, cnishim1
+
 CREATE TABLE Movie (
   movieID   VARCHAR(15),
   title     VARCHAR(300),
@@ -13,53 +16,95 @@ CREATE TABLE Movie (
 CREATE TABLE Genre (
   movieID   VARCHAR(15),
   genre     VARCHAR(100),
-  PRIMARY KEY (genre, movieID),
-  FOREIGN KEY (movieID) REFERENCES Movie(movieID)
+  PRIMARY KEY (movieID, genre),
+  FOREIGN KEY (movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Keyword (
   movieID   VARCHAR(15),
   keyword   VARCHAR(100),
-  PRIMARY KEY(keyword, movieID),
-  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+  PRIMARY KEY(movieID, keyword),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE ShootingLocation (
   movieID   VARCHAR(15),
-  address   VARCHAR(500),
+  locationListing  VARCHAR(500),
+  rawAddress  VARCHAR(500),
   city      VARCHAR(100),
   state     VARCHAR(100),
   country   VARCHAR(100),
-  PRIMARY KEY(movieID, address, city, state, country),
-  FOREIGN KEY(movieID) REFERENCES Movie(movieID)
+  PRIMARY KEY(movieID, locationListing, rawAddress, city, state, country),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Person (
   personID  VARCHAR(15),
   name      VARCHAR(200),
-  birthday  DATE,
+  birthYear  INT,
   PRIMARY KEY(personID)
 );
 
 CREATE TABLE DirectedBy (
-  personID  VARCHAR(15),
   movieID   VARCHAR(15),
-  name      VARCHAR(200),
-  PRIMARY KEY(personID, movieID),
-  FOREIGN KEY(personID) REFERENCES Person(personID),
-  FOREIGN KEY(movieID) REFERENCES Movie(movieID),
-  FOREIGN KEY(name) REFERENCES Person(name)
+  personID  VARCHAR(15),
+  PRIMARY KEY(movieID, personID),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(personID) REFERENCES Person(personID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Starring (
-  personID  VARCHAR(15),
   movieID   VARCHAR(15),
-  name      VARCHAR(200),
+  personID  VARCHAR(15),
   role      VARCHAR(200),
-  PRIMARY KEY(personID, movieID, role),
-  FOREIGN KEY(personID) REFERENCES Person(personID),
-  FOREIGN KEY(movieID) REFERENCES Movie(movieID),
-  FOREIGN KEY(name) REFERENCES Person(name)
+  PRIMARY KEY(movieID, personID, role),
+  FOREIGN KEY (movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (personID) REFERENCES Person(personID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-  
+CREATE TABLE Ranking (
+  movieID   VARCHAR(15),
+  rank      INT,
+  PRIMARY KEY(movieID, rank),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/movie-small.txt' INTO TABLE Movie
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/genre-small.txt' INTO TABLE Genre
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/keywords-small.txt' INTO TABLE Keyword
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/person-small.txt' INTO TABLE Person
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/directedby-small.txt' INTO TABLE DirectedBy
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/starring-small.txt' INTO TABLE Starring
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/shootinglocation-small.txt' INTO TABLE ShootingLocation
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/ranking-small.txt' INTO TABLE Ranking
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
--- setup.sql
+++ setup.sql
@@ -1,3 +1,7 @@
+-- Andy Chen, achen93
+-- Curtis Nishimoto, cnishim1
+
+
 CREATE TABLE Movie (
   movieID   VARCHAR(15),
   title     VARCHAR(300),
@@ -10,7 +14,98 @@ CREATE TABLE Movie (
   PRIMARY KEY(movieID)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/movie.txt' INTO TABLE Movie
+CREATE TABLE Genre (
+  movieID   VARCHAR(15),
+  genre     VARCHAR(100),
+  PRIMARY KEY (movieID, genre),
+  FOREIGN KEY (movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Keyword (
+  movieID   VARCHAR(15),
+  keyword   VARCHAR(100),
+  PRIMARY KEY(movieID, keyword),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE ShootingLocation (
+  movieID   VARCHAR(15),
+  locationListing  VARCHAR(500),
+  rawAddress  VARCHAR(500),
+  city      VARCHAR(200),
+  state     VARCHAR(200),
+  country   VARCHAR(200),
+  PRIMARY KEY(movieID, locationListing, rawAddress, city, state, country),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Person (
+  personID  VARCHAR(15),
+  name      VARCHAR(200),
+  birthYear  INT,
+  PRIMARY KEY(personID)
+);
+
+CREATE TABLE DirectedBy (
+  movieID   VARCHAR(15),
+  personID  VARCHAR(15),
+  PRIMARY KEY(movieID, personID),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(personID) REFERENCES Person(personID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Starring (
+  movieID   VARCHAR(15),
+  personID  VARCHAR(15),
+  role      VARCHAR(200),
+  PRIMARY KEY(movieID, personID, role),
+  FOREIGN KEY (movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (personID) REFERENCES Person(personID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Ranking (
+  movieID   VARCHAR(15),
+  rank      INT,
+  PRIMARY KEY(movieID, rank),
+  FOREIGN KEY(movieID) REFERENCES Movie(movieID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/movie.txt' INTO TABLE Movie
 FIELDS TERMINATED BY '\t'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/genre.txt' INTO TABLE Genre
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/keywords.txt' INTO TABLE Keyword
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/person_condensed.txt' INTO TABLE Person
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/directedby.txt' INTO TABLE DirectedBy
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/starring.txt' INTO TABLE Starring
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/shootinglocation.txt' INTO TABLE ShootingLocation
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/andyc/Documents/Johns Hopkins/Spring 2021/Databases/project/filmDB/finaldata/ranking.txt' INTO TABLE Ranking
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
