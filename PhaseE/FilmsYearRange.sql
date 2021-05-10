2fe5f415e8e01c72d11279dc1f093c56c0a7a983
CnishComputing <70718607+CnishComputing@users.noreply.github.com>
2021-05-10 16:25:08 -0400

    Update FilmsYearRange.sql

--- PhaseE/FilmsYearRange.sql
+++ PhaseE/FilmsYearRange.sql
@@ -1,14 +1,19 @@
-
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS FilmsYearRange //
 
-CREATE PROCEDURE FilmsYearRange(IN startYear year, IN endYear year)
+CREATE PROCEDURE FilmsYearRange(IN startYear VARCHAR(4), IN endYear VARCHAR(4))
 BEGIN
-	SELECT title, year
-	FROM Movie
-	WHERE year >= startYear AND year <= endYear
-	ORDER BY year ASC;
+        WITH TopGross AS (SELECT title, year, revenue_adj
+             FROM Movie
+             WHERE year >= startYear AND year <= endYear
+             ORDER BY revenue_adj DESC
+             LIMIT 50)
+        SELECT *
+        FROM TopGross
+        ORDER BY year ASC, revenue_adj DESC, title ASC;
 END; //
 
 DELIMITER ;
+
+
