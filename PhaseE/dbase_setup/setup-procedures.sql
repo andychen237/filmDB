d2c9eaa18b357b900ab96f1ea18d47b2a62f1b2d
Andy Chen <andych3n@yahoo.com>
2021-05-11 21:15:02 -0400

    took out query with unideal results

--- PhaseE/dbase_setup/setup-procedures.sql
+++ PhaseE/dbase_setup/setup-procedures.sql
@@ -406,23 +406,6 @@ END; //
 
 DELIMITER ;
 
--- TopFiveYear.sql
-
-DELIMITER //
-
-DROP PROCEDURE IF EXISTS TopFiveYear //
-
-CREATE PROCEDURE TopFiveYear(IN startYear year)
-BEGIN
-        SELECT title, revenue, revenue_adj
-	FROM Movie
-	WHERE revenue IS NOT NULL AND revenue_adj IS NOT NULL AND year = startYear
-	ORDER BY revenue_adj desc
-	LIMIT 5;
-END; //
-
-DELIMITER ;
-
 -- topNgenres.sql
 
 DELIMITER //
